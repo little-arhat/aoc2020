69bb1d1e48c5381cd91fddaa2ee94d2ef894cf64
Roma Sokolov <sokolov.r.v@gmail.com>
2020-12-27 17:47:04 +0100

    Add 5

--- src/larhat/aoc2020.clj
+++ src/larhat/aoc2020.clj
@@ -4,13 +4,23 @@
 (defn input [n]
   (slurp (format "resources/larhat/inp%d.txt" n)))
 
+(defn words [s]
+  (clojure.string/split s #"\s"))
+
+(defn lines [s]
+  (clojure.string/split s #"\n"))
+
+(defn phrases [s]
+  (clojure.string/split s #"\n\n"))
+
 (defn inp-words [n]
-  (-> (input n)
-    (clojure.string/split #"\s")))
+  (-> (input n) words))
 
 (defn inp-lines [n]
-  (-> (input n)
-    (clojure.string/split #"\n")))
+  (-> (input n) lines))
+
+(defn inp-phrases [n]
+  (-> (input n) phrases))
 
 (defn parse-int [n]
   (Integer. n))
@@ -99,6 +109,89 @@
       (find-trees slope (constantly false) 5)
       (find-trees slope (constantly false) 7)
       (find-trees slope #(odd? %) 1))))
+
+(def fields #{:byr :iyr :eyr :hgt :hcl :ecl :pid :cid})
+
+(defn get-field [s]
+  (-> s
+    (subs 0 3)
+    (keyword)))
+
+(defn get-fields [passport]
+  (->> passport
+    (map get-field)
+    (into #{})))
+
+(defn valid-passport [passport]
+  (let [passport-fields (get-fields passport)
+        diff (clojure.set/difference fields passport-fields)]
+    (or
+      (empty? diff)
+      (= #{:cid} diff))))
+
+(defn aoc-4 []
+  (->> (inp-phrases 4)
+    (map words)
+    (filter valid-passport)
+    (count)))
+
+(defn between [as min max]
+  (when as
+    (let [a (parse-int as)]
+      (and (>= a min) (<= a max)))))
+
+(defn split-str-backwards [s i]
+  (let [l (count s)
+        l' (- l i)]
+    [(subs s 0 l') (subs s l')]))
+
+(defn valid-height [s]
+  (when s
+    (let [[d m] (split-str-backwards s 2)]
+      (cond
+        (= "cm" m) (between d 150 193)
+        (= "in" m) (between d 59 76)
+        :else      false))))
+
+(def color-r #"^#[0-9a-f]{6}$")
+(def pid-r #"^\d{9}$")
+(def eyes #{:amb :blu :brn :gry :grn :hzl :oth})
+
+(defn safe-re-seq [r s]
+  (when s
+    (re-seq r s)))
+
+(def passport-rules
+  {:byr #(between % 1920 2002)
+   :iyr #(between % 2010 2020)
+   :eyr #(between % 2020 2030)
+   :hgt valid-height
+   :hcl #(safe-re-seq color-r %)
+   :ecl #(eyes (keyword %))
+   :pid #(safe-re-seq pid-r %)
+   :cid (constantly true)})
+
+(defn get-field-value [s]
+  (let [[f v] (clojure.string/split s #":")]
+    [(keyword f) v]))
+
+(defn get-fields-2 [passport]
+  (->> passport
+    (map get-field-value)
+    (into {})))
+
+(defn valid-passport-2 [passport]
+  (let [passport-fields (get-fields-2 passport)]
+    (every?
+      (fn [[k f]]
+        (f (k passport-fields)))
+     passport-rules)))
+
+(defn aoc-4-2 []
+  (->> (inp-phrases 4)
+    (map words)
+    (filter valid-passport-2)
+    (count)))
 
 (defn -main
   "I don't do a whole lot ... yet."
