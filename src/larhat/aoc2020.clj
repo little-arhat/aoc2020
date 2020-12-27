86bd407a275e9ac2f145d6ab8f87120c85d466e0
Roma Sokolov <sokolov.r.v@gmail.com>
2020-12-27 16:06:09 +0100

    add 3

--- src/larhat/aoc2020.clj
+++ src/larhat/aoc2020.clj
@@ -4,7 +4,11 @@
 (defn input [n]
   (slurp (format "resources/larhat/inp%d.txt" n)))
 
-(defn inp-seq [n]
+(defn inp-words [n]
+  (-> (input n)
+    (clojure.string/split #"\s")))
+
+(defn inp-lines [n]
   (-> (input n)
     (clojure.string/split #"\n")))
 
@@ -12,7 +16,7 @@
   (Integer. n))
 
 (defn aoc-1 []
-  (let [i (map parse-int (inp-seq 1))]
+  (let [i (map parse-int (inp-lines 1))]
     (first (for [x i
                  y i
                  :let  [sum (+ x y)]
@@ -20,7 +24,7 @@
              (* x y)))))
 
 (defn aoc-1-2 []
-  (let [i (map parse-int (inp-seq 1))]
+  (let [i (map parse-int (inp-lines 1))]
     (first (for [x i
                  y i
                  z i
@@ -29,7 +33,7 @@
              (* x y z)))))
 
 (def pass-r #"(\d+)-(\d+) (\w): (\w+)")
-(defn parse-password [p]
+(defn parse-pass [p]
   (let [[_ min max char pass] (re-find pass-r p)]
     {:min (parse-int min)
      :max (parse-int max)
@@ -43,16 +47,58 @@
       (>= c min)
       (<= c max))))
 
-(defn valid-pass-2 [:keys [pos-1 pos-2 char pass]]
-  (or
-    (= char (.charAt pass (+1 pos-1)))
-    (= char (.charAt pass (+1 pos-2)))))
+(defn valid-pass-2 [{:keys [min max char pass]}]
+  (not=
+    (= char (.charAt pass (dec min)))
+    (= char (.charAt pass (dec max)))))
 
 (defn aoc-2 []
-  (->> (inp-seq 2)
-    (map parse-password)
+  (->> (inp-lines 2)
+    (map parse-pass)
     (filter valid-pass)
     (count)))
+
+(defn aoc-2-2 []
+  (->> (inp-lines 2)
+    (map parse-pass)
+    (filter valid-pass-2)
+    (count)))
+
+(defn mod+ [a b m]
+  (mod (+ a b) m))
+
+(defn find-path [map discard? step]
+  (let [width (count (first map))]
+    (reduce
+      (fn [{:keys [pos trees coord]} line]
+        (let [pos'   (mod+ pos step width)
+              ch     (.charAt line pos')
+              coord' [pos' ch line]
+              trees' (if (= ch \#) (inc trees) trees)]
+          {:pos   pos', :trees trees',
+           :coord (conj coord coord')}))
+      {:pos 0, :trees 0, :coord [] }
+      (keep-indexed (fn [index elem]
+                      (if (or (= 0 index) (discard? index))
+                        nil
+                        elem))
+        map))))
+
+(defn find-trees [map discard? step]
+  (:trees (find-path map discard? step)))
+
+(defn aoc-3 []
+  (let [map (inp-lines 3)]
+    (find-trees map #(= 0 %) 3)))
+
+(defn aoc-3-2 []
+  (let [slope (inp-lines 3)]
+    (*
+      (find-trees slope (constantly false) 1)
+      (find-trees slope (constantly false) 3)
+      (find-trees slope (constantly false) 5)
+      (find-trees slope (constantly false) 7)
+      (find-trees slope #(odd? %) 1))))
 
 (defn -main
   "I don't do a whole lot ... yet."
